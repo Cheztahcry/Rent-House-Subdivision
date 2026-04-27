8953f132ebf3cedb4ccbca7e329ea654391f3013
Chester Carey Rojo <chstrcryrj@gmail.com>
2026-04-27 23:16:55 +0800

    refactor the logic

--- database.php
+++ database.php
@@ -1,27 +1,64 @@
 <?php
-    $db_host = "localhost";
-    $db_user = "root";
-    $db_pass = "";
-    $db_name  = "dbhouserent";
     $tb_name = "rent_house";
     $conn = "";
+    $config = require 'config.php';
     $attribute_1 = "id";
     $attribute_2 = "lname";
     $attribute_3 = "fname";
     $attribute_4 = "gender";
     $attribute_5 = "age";
 
+    class DataBase{
+        private $pdo;
+        public function __construct(
+            private string $host,
+            private string $dbname,
+            private string $user,
+            private string $password
+              
+        ){
+            $dsn = "mysql:host=$this->host";
+            $options = [
+            PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES   => false,
+            ];
 
-    // Error handling for connection errors
-    try {
-        $dsn = "mysql:host=$db_host";
-    $pdo = new PDO($dsn, $db_user, $db_pass);
-    }catch (PDOException $e) {
-    error_log($e->getMessage()); 
-    die("Internal Server Error. Please try again later."); 
-}
+            try {
+                $this->pdo = new PDO($dsn, $this->user, $this->password);
+                $this->pdo->exec("CREATE DATABASE IF NOT EXISTS `{$this->dbname}` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci");
+                $this->pdo->exec("USE `{$this->dbname}` ");
+            }
+            catch (\PDOException $e) {
+                throw new \PDOException($e->getMessage(), (int)$e->getCode());
+
+            }
+        }
+        public function create_table($table, array $column){
+            $tabledat = [];
+            foreach($column as $name => $data_type){
+                $tabledat[] = "`$name` $data_type";
+            }
+                $create_table = "CREATE TABLE `$table` (" . implode(', ', $tabledat) . ")";
+                $this->pdo->exec($create_table);
+        }
+
+        public function insert_table($table, $att_2, $att_3, $att_4, $att_5){
+            $insert_info = "INSERT INTO `$table` (`lname`, `fname`, `age`, `gender`) VALUES(:lname, :fname, :age, :gender)";
+            $insert_info_query = $pdo->prepare($insert_info);
+            $insert_info_query->execute([
+            'lname' => $att_2, 
+            'fname' => $att_3, 
+            'age' => $att_4,
+            'gender' => $att5
+            ]);
+        }
 
 
+
+
+
+    }
     $lname = trim(($_POST['lname'] ?? null));
     $fname = trim(($_POST['fname'] ?? null));
     $gender = trim(($_POST['gender'] ?? null));
@@ -32,7 +69,7 @@
                   ];
     $errors = [];
     
-    // Check for empty fields; if there is an empty field, add it to the error list
+    // Check for empty fields; If their is empty field add it to the error list
     foreach ($owner_info as $info => $errorMessage) {
     if (empty(trim($_POST[$info] ?? ''))) {
         $errors[$info] = $errorMessage;
@@ -40,31 +77,26 @@
         
     }
     }
-    // If there is no errors(User Input) left, create the database, table, and insert the data
-    if (empty($errors)) {
-        echo "Successfully submitted!";
-        $create_database = "CREATE DATABASE IF NOT EXISTS `$db_name` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci";
-        $use_db = "USE `$db_name`";
-        $pdo->exec($create_database);
-        $pdo->exec($use_db);
-        $create_table = "CREATE TABLE IF NOT EXISTS `$tb_name`(
-        `$attribute_1` INT AUTO_INCREMENT PRIMARY KEY,
-        `$attribute_2` VARCHAR(50) NOT NULL,
-        `$attribute_3` VARCHAR(50) NOT NULL,
-        `$attribute_4` VARCHAR(50) NOT NULL,
-        `$attribute_5` INT(4) NOT NULL)ENGINE=InnoDB;";
-        $pdo->exec($create_table);
-        $insert_info = "INSERT INTO `$tb_name` (`lname`, `fname`, `age`, `gender`) VALUES(:lname, :fname, :age, :gender)";
-        $insert_info_query = $pdo->prepare($insert_info);
-        $insert_info_query->execute([
-            'lname' => $lname, 
-            'fname' => $fname, 
-            'age' => $age,
-            'gender' => $gender
-            ]);
-    }
-   
 
+    $field = [
+       'id' => 'INT AUTO_INCREMENT PRIMARY KEY',
+       'lname' => 'VARCHAR(50) NOT NULL',
+       'fname' => 'VARCHAR(50) NOT NULL',
+       'age' => 'INT(4) NOT NULL',
+       'gender' => 'VARCHAR(50) NOT NULL'
+    ];
+
+    // If there is no errors(User Input) left, create database, table, and insert the data
+    if (empty($errors)) {
+        $database = new Database(
+            $config['host'],
+            $config['db'],
+            $config['user'],
+            $config['password']
+        );
+        $database->create_table($tb_name, $field);
+        echo "Successfully submitted!";
+    }
 
 
 ?>
