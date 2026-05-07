ed3a30560c6eba59aa04afb0075faa9973b29f9f
mjmjmjmj00 <mjexconde00@gmail.com>
2026-05-07 10:08:50 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,75 @@
+<?php 
+include 'database.php';
+$config = require 'config.php';
+$visit_time = date("Y-m-d H:i:s");
+setcookie("one minute timer", "running", time() + 60, "/");
+$owners = new Database(
+            $config['host'],
+            $config['db'],
+            $config['user'],
+            $config['password']
+        );
+$rows = $owners->show_table($tb_name)
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/index.css">
+    
+</head>
+<header>
+  
+    <img src="assets/img/logo.png" class="logo" alt="Subdivision Logo">
+
+    <a href="index.php" class="signin-btn">RHS</a>
+    <a href="register.php" class="signin-btn">REGISTER MY HOUSE</a>
+    <a href="login.php" class="signin-btn">LOG-IN</a>
+</header>
+<html>
+    <footer>
+<img src="assets/img/logo.png" class="logo" alt="Subdivision Logo">
+</footer>
+</html>
+<body>
+    <div class = "dashboard-container">
+        
+        <table>
+        <thead>
+        <tr>
+            <th>Owner ID</th>
+            <th>Block Number</th>
+            <th>Lot Number</th>
+            <th>Owner</th>
+            <th>Rent Status</th>
+            <th>Current Owner</th>
+            <th>Rent Prize</th>
+            <th>Down Payment</th>
+            <th>Contact Number</th>
+
+    </tr>
+    <tbody>
+        <?php foreach ($rows as $row): ?>
+        
+                
+                        <tr>
+                        <td><?= $row->id ?></td>
+                        <td><?= $row->blocknumber ?></td>
+                        <td><?= $row->lotnumber ?></td>
+                        <td><?= $row->fname ?> <?= $row->lname?></td>
+                        <td>For Rent</td>
+                        <td><?= $row->fname ?> <?= $row->lname?></td>
+                        <td><?= $row->rentprice ?></td>
+                        <td><?= $row->downpayment?></td>
+                        <td><?= $row->contactnumber?></td>
+                        </tr>
+                
+        <?php endforeach; ?>
+        </tbody>
+        </table>
+        
+</body>
+</html>
+
